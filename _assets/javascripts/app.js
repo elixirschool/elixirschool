896ffbf8c7c8afa16ae1b5d84d6685f8869f41a7
Sean Callan <seancallan@gmail.com>
2015-11-28 17:01:18 -0800

    Collapsible sections

---
+++ _assets/javascripts/app.js
@@ -0,0 +1,23 @@
+/*
+ *= require jquery
+ */
+
+var swap_icon = function(el) {
+  el.find('i')
+    .toggleClass('fa-chevron-down')
+    .toggleClass('fa-chevron-right');
+};
+
+var toggle_lessons = function(el) {
+  el.children('.sidebar-nav-list').toggle();
+};
+
+var toggle_section = function() {
+  var parent = $(this).parent();
+  swap_icon(parent);
+  toggle_lessons(parent);
+};
+
+$(function() {
+  $('.sidebar-nav-header').click(toggle_section);
+});
