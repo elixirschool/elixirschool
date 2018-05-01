60130ce142b1080494078e8b560df91d76c064d6
George Mantzouranis <gemantzu@gmail.com>
2018-05-01 17:24:01 +0300

    version report

--- _plugins/elixir_school.rb
+++ _plugins/elixir_school.rb
@@ -21,9 +21,43 @@ module ElixirSchool
         site.config['contents'][lang] = contents(site, lang)
         site.config['interlang_names'][lang] = interlang_names(site, lang, " / ")
       end
+
+      # Build Report for each translation
+
+      site.config['translation_report'] = {}
+      site.config['tree'].each do |lang, content|
+        build_translation_report_defaults(site, default_lang, lang)
+        content.each do |section, section_content|
+          section_content.each do |lesson, lesson_content|
+            site.config['translation_report'][lang][section]['lessons'][lesson]['translated_title'] = lesson_content['title']
+            site.config['translation_report'][lang][section]['lessons'][lesson]['translated_version'] = pretify_version(lesson_content['version'])
+            site.config['translation_report'][lang][section]['lessons'][lesson]['version_severity'] = lesson_content['version_severity']
+          end
+        end
+      end
     end
 
     private
+    def pretify_version(version)
+      version.is_a?(Array) ? version.join('.') : ''
+    end
+
+    def build_translation_report_defaults(site, default_lang, lang)
+      site.config['translation_report'][lang] = {}
+      site.config['contents'][default_lang].each do |section, section_content|
+        site.config['translation_report'][lang][section] = {'lessons' => {}}
+        section_content.each do |lesson, lesson_content|
+          site.config['translation_report'][lang][section]['lessons'][lesson] = {
+            'lesson' => lesson_content['title'],
+            'translated_title' => '',
+            'original_version' => pretify_version(lesson_content['version']),
+            'translated_version' => '',
+            'version_severity' => 'error'
+          }
+        end
+      end
+    end
+
     def build_tree(site)
       site.config['tree'] = {}
       default_lang = site.config['default_lang']
